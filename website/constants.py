0314b51c08c0c26542d242a73f2c266c71a68479
me-gusta <you@example.com>
2023-11-02 14:08:54 +0400

    Add CONTENT_FOLDER automatic creation

--- website/constants.py
+++ website/constants.py
@@ -5,6 +5,7 @@ WEBSITE_NAME = 'Шаблон'
 TELEGRAM_OT_TOKEN = ''
 DOMAIN = 'http://127.0.0.1:5000'
 CONTENT_FOLDER = Path(__file__).parent.parent / 'content'
+CONTENT_FOLDER.mkdir(exist_ok=True)
 
 def get_global_info():
     return {
